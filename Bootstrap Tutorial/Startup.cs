714c6bfe7385ec2f3395c03516f02617721a832c
Jamie Element <sc12jcce@leeds.ac.uk>
2016-03-01 11:40:53 +0000

    Set up ASP.Net files from Visual Studio Template

---
+++ Bootstrap Tutorial/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bootstrap_Tutorial.Startup))]
+namespace Bootstrap_Tutorial
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
