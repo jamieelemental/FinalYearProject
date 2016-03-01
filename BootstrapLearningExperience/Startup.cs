8d25d6c61b12a30d679ad3ca73f01b2f3262bd50
Jamie Element <sc12jcce@leeds.ac.uk>
2016-03-01 17:25:47 +0000

    Regenerated template files.

---
+++ BootstrapLearningExperience/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BootstrapLearningExperience.Startup))]
+namespace BootstrapLearningExperience
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
