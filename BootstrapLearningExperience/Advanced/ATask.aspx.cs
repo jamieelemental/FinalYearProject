e1b8a00ef2d41a0e235c95fec1db423f476e0ad1
Jamie Element <sc12jcce@leeds.ac.uk>
2016-04-15 11:45:55 +0100

    Removed unused comment.

--- BootstrapLearningExperience/Advanced/ATask.aspx.cs
+++ BootstrapLearningExperience/Advanced/ATask.aspx.cs
@@ -47,7 +47,6 @@ namespace BootstrapLearningExperience.Advanced
             request.ContentType = "text/html; charset=UTF-8";
             request.Method = "POST";
 
-            //string prePostData = "<!DOCTYPE html><html><head><title>Users Code</title><meta charset = \"utf-8\" /><script src = \"https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\" ></script><script src = \"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\" ></script><link href = \"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\" rel = \"stylesheet\" /></head><body>";
             string postData = this.inpt.Text;
             byte[] byteArray = Encoding.UTF8.GetBytes(postData);
             request.ContentLength = byteArray.Length;
